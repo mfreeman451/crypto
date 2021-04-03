bd3f6fd5893e889164d9fb7945c35aa661184ec8
Michael Freeman <mfreeman451@gmail.com>
2021-04-03 17:22:25 -0500

    icleanup

--- tvlStats/t.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST -H "Accept: application/json" -H "X-Access-Token: MWFlMzE3NGM5NGUxOThhNjI1NGU5MDk0NzNiZTRhN2JlNGUxNWQyYmMyYjZmZTdjZTI2MDFmMWE0YmRlN2Y2Nw==" -H "Cache-Control: no-cache" -d '{ "frames": [ { "text": "$ ", "icon": null } ] }' --insecure https://192.168.1.128:4343/api/v1/dev/witget/update/com.lametric.a4b1372fbc776e9409ff666a0ec30503	
